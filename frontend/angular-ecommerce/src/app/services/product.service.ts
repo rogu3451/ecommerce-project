cbc86209cd439bca64fc3c66eda7a2cdef369dc4
Karol Rogozinski <karol.rogozinski97@gmail.com>
2021-02-21 13:38:08 +0100

    FRONTEND/BACKEND | Searching by category has been created

--- frontend/angular-ecommerce/src/app/services/product.service.ts
+++ frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -15,9 +15,10 @@ export class ProductService {
 
   getProductList(theCategoryId: number): Observable<Product[]>{
 
-    // @TODO: need to build URL based in category id ... will come back to this!
+    // need to build URL based in category id
+    const  searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
